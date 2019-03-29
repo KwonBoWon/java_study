04c567095eb96c6e4fd3813a088c7b97ca07370d
kwonbowon <bogod1207@naver.com>
2019-03-29 14:47:30 +0900

    구구단

---
+++ Main.java
@@ -0,0 +1,18 @@
+package pack;
+
+public class Main {
+	public static void main(String[] args) {
+		int a = 0;
+		for (int i = 1; i < 10; i++) {
+
+			for (int n = 1; n < 10; n++) {
+
+				System.out.println(i+"*"+n+"="+i*n);
+
+			}
+			
+
+		}
+	}
+
+}
